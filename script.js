eaa814e36f1bde2ab96e6c804105c27b6526a25b
excelekechi-00 <excel.ekechi@edu.immaginazioneelavoro.it>
2025-12-10 00:33:14 +0100

    hh

---
+++ script.js
@@ -0,0 +1,8 @@
+ore = 999;
+
+function assenze() {
+    n = parseInt(document.getElementById("progresso").value);
+    console.log(n);;
+    let assenze = n * 6;
+    let ore_assenti = ore - assenze
+}
